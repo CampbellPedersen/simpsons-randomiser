00a59863e5843aafb0c2cecf289f28658d34facb
Campbell <campbelljip@gmail.com>
2021-10-02 09:18:38 +1000

    Fix all episodes bug

--- src/services/disneyplus.ts
+++ src/services/disneyplus.ts
@@ -4,9 +4,10 @@ import { episodes } from '@src/data/episodes';
 import { Settings } from '@src/models/settings';
 
 export function openRandomEpisode(settings: Settings): void {
+  const {seasons, treeHouseOfHorror} = settings.filters;
   const filteredEpisodes = episodes
-    .filter(episode => settings.filters.seasons.includes(episode.season))
-    .filter(episode => !settings.filters.treeHouseOfHorror && !episode.title.includes('Treehouse of Horror'))
+    .filter(episode => !seasons.length || seasons.includes(episode.season))
+    .filter(episode => !treeHouseOfHorror && !episode.title.includes('Treehouse of Horror'))
 
   const episode = randomItem(filteredEpisodes);
   const url = `https://www.disneyplus.com/video/${episode.contentId}`;
